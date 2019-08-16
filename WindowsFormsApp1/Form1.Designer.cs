a8c07e753308877719964611dbce54f0a6fb33cf
暴走鱼 <bzy734084360@hotmail.com>
2019-08-16 12:11:40 +0800

    添加项目文件。

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,238 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtServer = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtUid = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtpwd = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtResultOrErrMsg = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtdatasource = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.lblbit = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.cmbDataType = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // txtServer
+            // 
+            this.txtServer.Location = new System.Drawing.Point(55, 42);
+            this.txtServer.Name = "txtServer";
+            this.txtServer.Size = new System.Drawing.Size(100, 21);
+            this.txtServer.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(8, 46);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "server";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(161, 47);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(23, 12);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "uid";
+            // 
+            // txtUid
+            // 
+            this.txtUid.Location = new System.Drawing.Point(190, 43);
+            this.txtUid.Name = "txtUid";
+            this.txtUid.Size = new System.Drawing.Size(100, 21);
+            this.txtUid.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(297, 48);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(23, 12);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "pwd";
+            // 
+            // txtpwd
+            // 
+            this.txtpwd.Location = new System.Drawing.Point(326, 44);
+            this.txtpwd.Name = "txtpwd";
+            this.txtpwd.Size = new System.Drawing.Size(100, 21);
+            this.txtpwd.TabIndex = 3;
+            this.txtpwd.TextChanged += new System.EventHandler(this.TextBox3_TextChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 83);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(53, 12);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "连接结果";
+            // 
+            // txtResultOrErrMsg
+            // 
+            this.txtResultOrErrMsg.Location = new System.Drawing.Point(71, 83);
+            this.txtResultOrErrMsg.Multiline = true;
+            this.txtResultOrErrMsg.Name = "txtResultOrErrMsg";
+            this.txtResultOrErrMsg.Size = new System.Drawing.Size(446, 150);
+            this.txtResultOrErrMsg.TabIndex = 6;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(71, 266);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "打开连接";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(285, 266);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "关闭连接";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(432, 46);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(65, 12);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "datasource";
+            // 
+            // txtdatasource
+            // 
+            this.txtdatasource.Location = new System.Drawing.Point(503, 42);
+            this.txtdatasource.Name = "txtdatasource";
+            this.txtdatasource.Size = new System.Drawing.Size(100, 21);
+            this.txtdatasource.TabIndex = 4;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(8, 9);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(137, 12);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "当前程序编译版本位数：";
+            // 
+            // lblbit
+            // 
+            this.lblbit.AutoSize = true;
+            this.lblbit.Location = new System.Drawing.Point(151, 9);
+            this.lblbit.Name = "lblbit";
+            this.lblbit.Size = new System.Drawing.Size(41, 12);
+            this.lblbit.TabIndex = 13;
+            this.lblbit.Text = "server";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(618, 47);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(65, 12);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "数据库类型";
+            // 
+            // cmbDataType
+            // 
+            this.cmbDataType.FormattingEnabled = true;
+            this.cmbDataType.Items.AddRange(new object[] {
+            "Oracle",
+            "SqlServer",
+            "MySql"});
+            this.cmbDataType.Location = new System.Drawing.Point(689, 43);
+            this.cmbDataType.Name = "cmbDataType";
+            this.cmbDataType.Size = new System.Drawing.Size(99, 20);
+            this.cmbDataType.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 348);
+            this.Controls.Add(this.cmbDataType);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.lblbit);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtdatasource);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtResultOrErrMsg);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtpwd);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtUid);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtServer);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtServer;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtUid;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtpwd;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtResultOrErrMsg;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtdatasource;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label lblbit;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.ComboBox cmbDataType;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OracleClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        //打开数据库连接  
+        public bool OpenOracle()
+        {
+            //从配置文件中获取连接字符串  
+            //配置文件需要放在项目目录下的bin\Release中  
+            //不做配置连接
+            string connString = $"Data Source={txtdatasource.Text};Persist Security Info=True;User ID={txtUid.Text};Pwd={txtpwd.Text};Unicode=True;Pooling=true;Max Pool Size=400;enlist=true;";
+            OracleConnection conn = new OracleConnection(connString);
+            try
+            {
+                conn.Open();
+                txtResultOrErrMsg.Text = "数据库连接成功";
+                //OracleCommand cmd = new OracleCommand();
+                return true;
+            }
+            catch (System.Exception ex)
+            {
+                txtResultOrErrMsg.Text = $"数据库连接失败，失败原因：{ex.Message}";
+                return false;
+            }
+        }
+
+        public void Close()
+        {
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            switch (cmbDataType.Text)
+            {
+                case "Oracle":
+                    this.OpenOracle();
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        bool IsApp64()
+        {
+            if (IntPtr.Size == 8)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            lblbit.Text = this.IsApp64() == true ? "64位" : "32位";
+        }
+    }
+}
